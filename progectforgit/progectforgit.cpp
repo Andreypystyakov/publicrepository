acdecaf02636315d69917babe23ac2e45b88a88d
Andreypystyakov <andrey227678@mail.ru>
2023-02-28 22:22:18 +0300

    2 изменение с программой для нахождения корней уравнения

--- progectforgit/progectforgit.cpp
+++ progectforgit/progectforgit.cpp
@@ -1,6 +1,33 @@
-﻿#include <iostream>
-
-int main()
+﻿# include <stdio.h>
+# include <locale.h>
+# include <math.h> /*Подключение математической библиотеки */
+/*Вычисление значений функции */
+int main()// программа для вычисления корней уравнения с известными коэффициентами
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+	int A, B, C;
+	float x1, x2, D, kD, x;
+	A = 1;
+	B = 5;
+	C = 6;
+	D = B * B - 4 * A * C;
+	if (D > 0)
+	{
+		kD = sqrt(D);
+		x1 = -1 * B + kD;
+		x1 = x1 / 2 / A;
+		x2 = -1 * B - kD;
+		x2 = x2 / 2 / A;
+		printf_s("x1 = %f, x2 = %f\n", x1, x2);
+	}
+	else
+	{
+		if (D == 0)
+		{
+			x = -1 * B;
+			x = x / 2 / A;
+			printf_s("x = %f\n", x);
+		}
+		else printf_s("решений нет\n");
+	}
 }
